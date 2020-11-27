e916b1a336ba6d82324153cd20a520474c26221c
JackDaniel <73126214+JackDaniel26@users.noreply.github.com>
2020-11-27 22:17:39 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,7 +1,7 @@
 import asyncio
 from telethon import events
 from userbot.utils import admin_cmd, sudo_cmd
-from userbot import ALIVE_NAME, hellversion
+from userbot import ALIVE_NAME, alphaversion
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
 
@@ -10,19 +10,19 @@ kraken = bot.uid
 PM_IMG = "https://i.ibb.co/Swvg82s/IMG-20201029-205430-067.jpg"
 pm_caption = "__**🔥🔥Aliveɮօt ɨs օռʟɨռɛ🔥🔥**__\n\n"
 
-pm_caption += f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**『[{DEFAULTUSER}](tg://user?id={kraken})』**\n\n"
+pm_caption += f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**『[{DEFAULTUSER}](tg://user?id={alphacracker01})』**\n\n"
 
 pm_caption += "🛡️TELETHON🛡️ : 1.15.0 \n"
 
-pm_caption += f"😈Hêllẞø†😈       : `{hellversion}`\n"
+pm_caption += f"😈Hêllẞø†😈       : `{Alphaversion}`\n"
 
-pm_caption += "⚠️CHANNEL⚠️   : [ᴊᴏɪɴ](https://t.me/HellBot_Official)\n"
+pm_caption += "⚠️CHANNEL⚠️   : [ᴊᴏɪɴ](https://t.me/httpstmejoinchatKRMA6RuflY)\n"
 
-pm_caption += "🔱GROUP🔱.       : [ᴊᴏɪɴ](https://t.me/HellBot_Official_Chat)\n"
+pm_caption += "🔱GROUP🔱.       : [ᴊᴏɪɴ](https://t.me/httpstmejoinchatKRMA6RuflY)\n"
 
-pm_caption += "🔥CREATOR🔥    : [Nub Here](https://t.me/kraken_the_badass)\n\n"
+pm_caption += "🔥CREATOR🔥    : [Nub Here](https://t.me/alphacracker01)\n\n"
 
-pm_caption += "         [✨REPO✨](https://github.com/hellboy-op/hellbot) 🔹 [📜License📜](https://github.com/HellBoy-OP/HellBot/blob/master/LICENSE)"
+pm_caption += "         [✨REPO✨](https://github.com/JackDaniel26/Alphauserbot) 🔹 [📜License📜](https://github.com/JackDaniel26/Alphauserbot/blob/master/LICENSE)"
 #@command(outgoing=True, pattern="^.alive$")
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
 @bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
